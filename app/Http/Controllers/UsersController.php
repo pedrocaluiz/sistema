12efe2be6149f23fd63fe54ec763097c82608990
pedrocaluiz <50851668+pedrocaluiz@users.noreply.github.com>
2019-08-12 14:43:19 -0300

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -308,24 +308,41 @@ class UsersController extends Controller
     public function update(Request $request, $id)
     {
         DB::beginTransaction();
-        $curso = Curso::find($id);
-
-        $this->authorize('update', $curso);
-
-        $curso->categoria_id = $request->input('categoria_id');
-        $curso->titulo = $request->input('tituloCurso');
-        $curso->descricao = $request->input('descricaoCurso');
-        $curso->icone = $request->input('icone');
-        $curso->palavrasChave = $request->input('palavrasChave');
-        $curso->usuarioAtualizacao = $request->input('usuarioAtualizacao');
-        $curso->ativo = 0;
-        $curso->save();
+        $usuario = User::find($id);
+        
+		$usuario->primeiroNome = $request->input('primeiroNome');
+		$usuario->ultimoNome = $request->input('ultimoNome');
+		if ($request->input('password') != null){
+			$usuario->password = Hash::make($request->input('ultimoNome'));
+		}
+		if ($request->input('foto') != null){
+			$path = $request->input('foto')->store('imagens', 'public');
+			$usuario->foto = $path;
+		}
+		$usuario->cargo_id = $request->input('cargo_id');
+		$usuario->funcao_id = $request->input('funcao_id');
+		$usuario->agencia_id = $request->input('agencia_id');
+		$usuario->matricula = $request->input('matricula');
+		if ($request->input('dataNascimento') != null){
+			$usuario->dataNascimento = $request->input('dataNascimento');
+		}
+		if ($request->input('dataAdmissao') != null){
+			$usuario->dataAdmissao = $request->input('dataAdmissao');
+		}
+		$usuario->endereco = $request->input('endereco');
+		$usuario->numero = $request->input('numero');
+		$usuario->complemento = $request->input('complemento');
+		$usuario->CEP = $request->input('CEP');
+		$usuario->municipio_id = $request->input('municipio_id');
+		$usuario->telefone = $request->input('telefone');
+		$usuario->celular = $request->input('celular');
+		$usuario->save();
 
         $request->session()->flash('alterada',
-            "Curso $curso->titulo alterado com sucesso.");
+            "Dados alterados com sucesso.");
         DB::commit();
 
-        return redirect()->route('cursos');
+        return redirect()->route('meu-perfil');
     }
 
 }

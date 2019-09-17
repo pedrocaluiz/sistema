453b611233360e3964a2cd14ad4679ba0d383876
Pedro Luiz Colognese <pedrocaluiz@hotmail.com>
2019-09-16 22:44:29 -0300

    AdminLTE

--- resources/views/layouts/subviews/head.blade.php
+++ resources/views/layouts/subviews/head.blade.php
@@ -8,7 +8,7 @@
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
 <!-- Bootstrap 3.3.7 -->
-<link rel="stylesheet" href=" {{asset('adminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href=" {{asset('css/adminLTE/bootstrap.min.css')}}">
 <!-- Font Awesome -->
 <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
 <link rel="stylesheet" href="{{asset('adminLTE/bower_components/font-awesome/css/font-awesome.min.css')}}">

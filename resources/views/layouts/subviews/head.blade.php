9ec330d8b0618d65d8cba83e6a64e1302c015ce2
Pedro Luiz Colognese <pedrocaluiz@hotmail.com>
2019-09-17 20:58:31 -0300

    Mudanças no Head, alteração dos arquivos AdminLTE

--- resources/views/layouts/subviews/head.blade.php
+++ resources/views/layouts/subviews/head.blade.php
@@ -17,7 +17,7 @@
 
 <link rel="stylesheet" href="{{asset('css/adminLTE/font-awesome/css/font-awesome.min.css')}}">
 <!-- Theme style -->
-<link rel="stylesheet" href="{{asset('css/adminLTE/dist/css/AdminLTE.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/adminLTE/css/AdminLTE.min.css')}}">
 <!-- AdminLTE Skins. Choose a skin from the css/skins
      folder instead of downloading all of them to reduce the load. -->
 <link rel="stylesheet" href="{{asset('css/adminLTE/css/skins/_all-skins.min.css')}}">
